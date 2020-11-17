61290949c1a1063aab4ea0767c77452eeffe2402
Bryan Coulier <bryan.coulier@hotmail.com>
2020-11-17 09:27:48 +0100

    travel page subscribe

--- controllers/travel.js
+++ controllers/travel.js
@@ -2,3 +2,8 @@
 exports.get_travel = function(req, res, next) {
     res.render('travel', { title: 'Express' });
   }
+
+exports.submit_subscriber = function(req, res, next) {
+    console.log("Email subscriber:", req.body.subscribe_email);
+    res.redirect('/');
+  }
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 let travel = require('../controllers/travel')
 /* GET home page. */
 router.get('/', travel.get_travel);
+router.post('/', travel.submit_subscriber);
 
 module.exports = router;
